e20bf140fd6064adc10fff978d151ae7dfc47983
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2023-09-08 17:46:04 +0530

    login backend1 commit

--- app/src/main/java/com/example/chatapplication/login.java
+++ app/src/main/java/com/example/chatapplication/login.java
@@ -1,15 +1,60 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
 
+    Button button;
+    EditText username,password;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.Loginbutton);
+        username = findViewById(R.id.editTextLogEmailAddress);
+        password = findViewById(R.id.editTextLogPassword);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginEmail = username.getText().toString();
+                String loginPass = password.getText().toString();
+
+                auth.signInWithEmailAndPassword(loginEmail,loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+//                            progressDialog.show();
+                            try{
+                                Intent intent = new Intent(login.this,MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }catch (Exception e){
+                                Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
