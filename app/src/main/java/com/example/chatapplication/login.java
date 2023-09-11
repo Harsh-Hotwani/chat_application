96eceb30a5eac36923e5bbed27d43d744b9161d4
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2023-09-11 23:48:16 +0530

    login backend1 commit

--- app/src/main/java/com/example/chatapplication/login.java
+++ app/src/main/java/com/example/chatapplication/login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,24 +37,40 @@ public class login extends AppCompatActivity {
             public void onClick(View v) {
                 String loginEmail = username.getText().toString();
                 String loginPass = password.getText().toString();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-                auth.signInWithEmailAndPassword(loginEmail,loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-//                            progressDialog.show();
-                            try{
-                                Intent intent = new Intent(login.this,MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }catch (Exception e){
-                                Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                if (TextUtils.isEmpty(loginEmail))
+                {
+                    Toast.makeText(login.this, "enter the email", Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(loginPass))
+                {
+                    Toast.makeText(login.this, "enter your password", Toast.LENGTH_SHORT).show();
+                } else if (!loginEmail.matches(emailPattern)) {
+                    username.setError("please enter a valid email");
+                } else if (loginPass.length()<6) {
+                    password.setError("more than 6 characters");
+                    Toast.makeText(login.this, "password must be greater than 6 characters", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    auth.signInWithEmailAndPassword(loginEmail, loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //                            progressDialog.show();
+                                try {
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } catch (Exception e) {
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
-                        }else{
-                            Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
+                    });
+                }
             }
         });
     }
--- app/src/main/java/com/example/chatapplication/registration.java
+++ app/src/main/java/com/example/chatapplication/registration.java
@@ -2,14 +2,46 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class registration extends AppCompatActivity {
+
+    TextView loginbut;
+    EditText rg_username , rg_email , rg_password, rg_repassword;
+    Button singup;
+    CircleImageView img;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         getSupportActionBar().hide();
+
+        rg_email=findViewById(R.id.rgemail);
+        rg_username=findViewById(R.id.rgusername);
+        rg_password=findViewById(R.id.rgpassword);
+        rg_repassword=findViewById(R.id.rgrepassword);
+        singup=findViewById(R.id.signupbutton);
+        img=findViewById(R.id.profilerg0);
+        loginbut=findViewById(R.id.loginbut);
+
+        loginbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(registration.this,login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
