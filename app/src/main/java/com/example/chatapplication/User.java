9eaab5d81375586116f4af7f44ef86a5b2b811b7
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2023-09-12 23:05:32 +0530

    getter setter complete commit

---
+++ app/src/main/java/com/example/chatapplication/User.java
@@ -0,0 +1,63 @@
+package com.example.chatapplication;
+
+public class User {
+    String profilepic,email,userName,password,userId,lastMessage,status;
+
+    public Users(){}
+
+    public String getProfilepic() {
+        return profilepic;
+    }
+
+    public void setProfilepic(String profilepic) {
+        this.profilepic = profilepic;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
