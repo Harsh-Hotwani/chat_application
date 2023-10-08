ca76799b00eb340249677c4fc78b83aa832beb84
Harsh-Hotwani <122560845+Harsh-Hotwani@users.noreply.github.com>
2023-10-08 21:44:10 +0530

    showing user in main screen commit

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -1,23 +1,65 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.ktx.Firebase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
+    RecyclerView mainUserRecyclerView;
+    UserAdapter userAdapter;
+    FirebaseDatabase database;
+    ArrayList<Users> usersArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
+        DatabaseReference reference = database.getReference().child("user");
+
+        usersArrayList=new ArrayList<>();
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot:snapshot.getChildren() )
+                {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    usersArrayList.add(users);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        mainUserRecyclerView = findViewById(R.id.mainUserRecyclerView);
+        mainUserRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userAdapter=new UserAdapter(MainActivity.this,usersArrayList);
+        mainUserRecyclerView.setAdapter(userAdapter);
+
         if (auth.getCurrentUser() == null)
         {
             Intent intent = new Intent(MainActivity.this,login.class);
--- app/src/main/java/com/example/chatapplication/UserAdapter.java
+++ app/src/main/java/com/example/chatapplication/UserAdapter.java
@@ -1,31 +1,58 @@
 package com.example.chatapplication;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.viewholder> {
+    MainActivity mainActivity;
+    ArrayList<Users> usersArrayList;
+    public UserAdapter(MainActivity mainActivity, ArrayList<Users> usersArrayList) {
+        this.mainActivity=mainActivity;
+        this.usersArrayList=usersArrayList;
+    }
+
     @NonNull
     @Override
     public UserAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mainActivity).inflate(R.layout.item,parent,false);
+        return new viewholder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull UserAdapter.viewholder holder, int position) {
 
+        Users users = usersArrayList.get(position);
+        holder.username.setText(users.userName);
+        holder.userstatus.setText(users.status);
+        Picasso.get().load(users.profilepic).into(holder.imageView);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return usersArrayList.size();
     }
 
     public class viewholder extends RecyclerView.ViewHolder {
+        CircleImageView imageView;
+        TextView username;
+        TextView userstatus;
         public viewholder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.profilerg0);
+            username = itemView.findViewById(R.id.username);
+            userstatus = itemView.findViewById(R.id.userstatus);
         }
     }
 }
